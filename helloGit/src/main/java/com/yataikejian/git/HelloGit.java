cced18d40da53ca046b28b7b1744955f7cffa9ab
dawn0391 <dawn0391@163.com>
2020-06-15 09:36:47 -0700

    添加文件

---
+++ helloGit/src/main/java/com/yataikejian/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.yataikejian.git;
+
+/**
+ * @author Daniel Li
+ * @creat 2020-06-15-9:15 AM
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
