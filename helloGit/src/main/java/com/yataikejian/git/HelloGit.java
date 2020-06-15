8e5a167aee952a26e15bed5eb3d189f7332a4b40
dawn0391 <dawn0391@163.com>
2020-06-15 15:20:21 -0700

    测试SSH

--- helloGit/src/main/java/com/yataikejian/git/HelloGit.java
+++ helloGit/src/main/java/com/yataikejian/git/HelloGit.java
@@ -13,6 +13,6 @@ public class HelloGit {
         System.out.println("分支添加");
         System.out.println("在线更新");
         System.out.println("克隆下来的项目添加");
-        System.out.println("测试SSH");
+        System.out.println("测试SSH1");
     }
 }
